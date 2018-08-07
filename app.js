9d799c01b4994fc7351f8fef2cd776254fcee72c
alevchenko <alevchenko@lohika.com>
2018-08-07 07:10:00 +0000

    added middleware and error handling

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const helloRoute = require('./routes/hello');
+const logger = require('./middleware/logger');
 const app = express();
 
 
 app.use(express.static('public'));
+app.use(logger({name: 'Custom'}));
+
+const requestTime = (req, res, next) => {
+    req.requestTime = Date.now();
+    next();
+}
+app.use(requestTime);
+
 app.use('/hello', helloRoute);
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!');
+})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
--- routes/hello.js
+++ routes/hello.js
@@ -3,8 +3,22 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello Node!')
+    let responseText = 'Hello World!<br>'
+    responseText += '<small>Requested at: ' + req.requestTime + '</small>'
+    res.send(responseText);
 });
+
+router.get('/err', async (req, res, next) => {
+    try {
+        await err();
+    } catch (e) {
+        next(e);
+    }
+});
+
+function err() {
+    return Promise.reject(Error('Rejected'));
+}
 
 router.post('/', (req, res) => {
     res.send('Got a POST request');
