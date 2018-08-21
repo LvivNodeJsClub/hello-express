781a9c6af5264c51c61f87ae9ab0b247468473be
alevchenko <alevchenko@lohika.com>
2018-08-21 09:15:38 +0000

    added new migration, new endpoints, transactions

--- routes/pizza.js
+++ routes/pizza.js
@@ -9,4 +9,25 @@ router.post('/', async (req, res) => {
     res.json(pizza);
 });
 
+router.post('/custom', async (req, res) => {
+    console.log('Request body', req.body);
+    const pizza = await pizzaService.saveCustom(req.body);
+    res.json(pizza);
+});
+
+router.get('/', async (req, res) => {
+    const pizzas = await pizzaService.findAll();
+    res.json(pizzas);
+});
+
+router.put('/', async (req, res) => {
+    const pizza = await pizzaService.update(req.query.id, req.body);
+    res.json(pizza);
+});
+
+router.delete('/', async (req, res) => {
+    const pizza = await pizzaService.delete(req.query.id);
+    res.json(pizza);
+});
+
 module.exports = router;
--- service/pizzaService.js
+++ service/pizzaService.js
@@ -2,8 +2,37 @@ const config = require('../knexfile');
 const knex   = require('knex')(config);
 
 class PizzaService {
+
     save(pizza) {
         return knex('pizza').insert(pizza);
+    }
+
+    findAll() {
+        return knex('pizza').select();
+    }
+
+    update(id, pizza) {
+        return knex('pizza').where({id}).update(pizza);
+    }
+
+    delete(id) {
+        return knex('pizza').where({id}).delete();
+    }
+
+    saveCustom(customPizza) {
+        const toppings = customPizza.toppings.map(name => ({name}));
+        const pizza = {...customPizza};
+        delete pizza.toppings;
+
+        return knex.transaction(async trx => {
+            const toppingIds = await trx('toppings').insert(toppings).returning('id');
+            console.log('toppingIds', toppingIds);
+            const [pizzaId] = await trx('pizza').insert(pizza).returning('id');
+            console.log('pizzaId', pizzaId);
+            const toppingToPizza = toppingIds.map(id => ({topping_id: id, pizza_id: pizzaId}));
+            return trx('pizzaToToppings').insert(toppingToPizza);
+        });
+
     }
 }
 
